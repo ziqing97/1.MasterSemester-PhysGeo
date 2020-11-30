b620833640b267b2811edc6e6979b88cf90323e6
ziqing97 <ziqing.yu0623@gmail.com>
2020-11-30 16:03:48 +0100

    update Code

--- Uebung1/Code/Aufgabe3.m
+++ Uebung1/Code/Aufgabe3.m
@@ -7,3 +7,19 @@ clear all
 close all
 
 % 
+% Drehwinkel
+gamma = pi / 12;
+% Laenge & Breite des Zentrums
+lambda_z = (9 + 11/60) / 180 * pi;
+phi_z = (48 + 46/60) /180 * pi;
+
+% P(6,4)
+theta = linspace(0,pi,181);
+P_all = Normalized_Lengendre(6,theta);
+P_64 = P_all{7,5};
+
+% Y
+lambda = linspace(-pi,pi,361);
+% to be continued
+
+
